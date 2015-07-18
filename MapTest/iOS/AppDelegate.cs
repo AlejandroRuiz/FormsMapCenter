e60ad6675711f5f77d3c292fe11c03ee4265b387
Alejandro Ruiz <elgoberlivel@gmail.com>
2015-07-17 21:38:16 -0500

    Xamarin Forms Maps init method implemented

--- MapTest/iOS/AppDelegate.cs
+++ MapTest/iOS/AppDelegate.cs
@@ -13,7 +13,7 @@ namespace MapTest.iOS
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
 			global::Xamarin.Forms.Forms.Init ();
-
+			global::Xamarin.FormsMaps.Init ();
 			// Code for starting up the Xamarin Test Cloud Agent
 			#if ENABLE_TEST_CLOUD
 			Xamarin.Calabash.Start();
